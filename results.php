7fee164e26e8884a43a70f679fdfc7fd254416c1
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-04-26 14:45:50 +0000

    Fixed results.php indentation

--- results.php
+++ results.php
@@ -6,42 +6,42 @@ define("ELDERLY_FREE", 95);
 define("STUDENT_MINIMUM", 12);
 define("STUDENT_MAXIMUM", 21);
 
-  //Define cost that will be displayed as result later on
+//Define cost that will be displayed as result later on
 $cost = "";
 
-  //Get the user's age and the day of the week from input form
+//Get the user's age and the day of the week from input form
 $userAge = intval($_POST["user-age"]);
 $day = $_POST["day"];
 
-  //If age is less than 0, display that they must enter a valid age
+//If age is less than 0, display that they must enter a valid age
 if ($userAge < 0) {
-  $cost = "Please enter a valid age.";
+    $cost = "Please enter a valid age.";
 } 
 
-  //If the user does not enter their age, say that they must enter their age
+//If the user does not enter their age, say that they must enter their age
 elseif (empty($userAge)) {
-  $cost = "Please enter age.";
+    $cost = "Please enter age.";
 } 
 
-  //Otherwise, if the user does not enter the day of the week, display that they must enter day of the week
+//Otherwise, if the user does not enter the day of the week, display that they must enter day of the week
 elseif ($day == "Day of week") {
-  $cost = "Please enter a day of the week.";
+    $cost = "Please enter a day of the week.";
 } 
 
-  //Otherwise, if the user is under 5 OR over 95 years old, display that they get in to the museum for free
+//Otherwise, if the user is under 5 OR over 95 years old, display that they get in to the museum for free
 elseif (($userAge < CHILD_FREE) || ($userAge > ELDERLY_FREE)) {
-  $cost = "You can visit the museum for free!";
+    $cost = "You can visit the museum for free!";
 } 
 
-  //Otherwise, if the user is between 12 and 21 years old, OR if it is Tuesday or Thursday, display that they get a discount
+//Otherwise, if the user is between 12 and 21 years old, OR if it is Tuesday or Thursday, display that they get a discount
 elseif ((($userAge >= STUDENT_MINIMUM) && ($userAge <= STUDENT_MAXIMUM)) || (($day == "Thursday") || ($day == "Tuesday"))) {
-  $cost = "You get a student discount.";
-} 
-
-  //Else, display that they do not get a discount at all
-else {
-  $cost = "You are not eligible for a discount and must pay the full price.";
+    $cost = "You get a student discount.";
 }
 
-  // Display the museum pass the user gets
+//Else, display that they do not get a discount at all
+else {
+    $cost = "You are not eligible for a discount and must pay the full price.";
+}
+
+// Display the museum pass the user gets
 echo "$cost";
